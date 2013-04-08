64a93a3abc9e286b111d84bd0940aa0b5626e6e0
Quang Ngo <quang@selu.com.au>
2013-04-08 13:38:22 +1000

    Project setup.

---
+++ GrettingsLibrary/GrettingsLibrary.h
@@ -0,0 +1,13 @@
+//
+//  GrettingsLibrary.h
+//  GrettingsLibrary
+//
+//  Created by Quang Ngo on 8/04/13.
+//  Copyright (c) 2013 Tonto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GrettingsLibrary : NSObject
+
+@end
---
+++ GrettingsLibrary/GrettingsLibrary.m
@@ -0,0 +1,13 @@
+//
+//  GrettingsLibrary.m
+//  GrettingsLibrary
+//
+//  Created by Quang Ngo on 8/04/13.
+//  Copyright (c) 2013 Tonto. All rights reserved.
+//
+
+#import "GrettingsLibrary.h"
+
+@implementation GrettingsLibrary
+
+@end
